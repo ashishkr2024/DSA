3b7ef2364f4f04d0d7e8db9b63824a618b6dbc9a
ashishkr2024 <ashish_kr@outlook.com>
2024-06-14 11:51:34 +0530

    optimise bubble sort algorithm

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -3,16 +3,22 @@ using namespace std;
 
 int main()
 {
-    int arr[]={7,6,2,3,9};
+    int arr[]={3,4,5,6,7,8};
     int n=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<n;i++)
     {
+        bool swapped=false;
         for(int j=0;j<n-i;j++)
         {
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j],arr[j+1]);
+                swapped=true;
             }
+        }
+        if (swapped=false)
+        {
+            break;
         }
     }
     for(int k=0;k<n;k++)
