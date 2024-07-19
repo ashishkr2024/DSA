960fc6add0f872ec2a05af609d8ae6ffce09fc16
ashishkr2024 <ashish_kr@outlook.com>
2024-07-19 09:58:11 +0530

    binary search using recursion

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
+void print (int *arr,int s,int e)
+{
+    
+   for(int i = s;i<=e;i++)
+    {
+        cout<<arr[i]<<" ";
+        
+    }
+    cout<<endl;
+}
 bool binarysearch(int *arr, int s,int e, int k)
 {
+    print(arr,s,e);
     if(s>e) return 0;
     int mid = s + (e-s)/2;
+    cout<<"value of mid is -->"<<arr[mid]<<endl;
     if(arr[mid]==k)return true;
     if(arr[mid]>k)
     return binarysearch(arr, s,mid-1,k);
@@ -12,8 +24,8 @@ bool binarysearch(int *arr, int s,int e, int k)
 }
 int main()
 {
-    int arr[]= {1,2,3,4,5,6};
-    int size =  6;
-    int key =  3;
-    cout<<" number is present or not -->"<<binarysearch(arr,0, 5,key);
+    int arr[]= {2,4,6,10,14,18,22,25,28,30,33,36,222};
+    int size =  13;
+    int key =  222;
+    cout<<" number is present or not -->"<<binarysearch(arr,0, size-1,key);
 }
