c56317e555cef91cc3fe585ff5da428149295423
ashishkr2024 <ashish_kr@outlook.com>
2024-07-25 00:31:01 +0530

    buble sort by recursion

---
+++ bubble.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void bubblesort(int *arr,int size)
+{
+    if(size==0||size==1)return;
+    for(int i = 0; i<size-1;i++)
+    {
+        if(arr[i]>arr[i+1])
+            swap(arr[i],arr[i+1]);
+    }
+    bubblesort(arr,size-1);
+}
+int main()
+
+{
+    int arr []={2,6,3,5,1};
+    int size = 5;
+    bubblesort(arr,size);
+    for(int i =0; i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
